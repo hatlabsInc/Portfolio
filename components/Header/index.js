a148e6663ff7c50ceb06df6565df0af246cba36f
DeagleOfficial <supratikofficial1@gmail.com>
2023-11-23 22:13:23 +0530

    Fixed top section

--- components/Header/index.js
+++ components/Header/index.js
@@ -19,7 +19,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
 
   return (
     <>
-      <Popover className="block tablet:hidden mt-5">
+      <Popover className="block tablet:hidden">
         {({ open }) => (
           <>
             <div className="flex items-center justify-between p-2 laptop:p-0">
