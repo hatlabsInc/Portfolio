8f1e9fb52bbc3053680d0e6304de45c70c11e6aa
ichbinprashant <mishraprashant304@gmail.com>
2023-11-24 00:06:26 +0530

    alignment

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -16,17 +16,17 @@ const Footer = ({}) => {
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold mb-3">
               together
             </h1>
-            <Button type="primary">Schedule a call</Button>
-            <div className="mt-10 ">
+            <Button type="primary" >Schedule a call</Button>
+            <div className="mt-20 ">
               <Socials />
             </div>
           </div>
         </div>
       </div>
-      <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
+      <h1 className="text-xs mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{" "}
         <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Hatlabsa</a>
+          <a className="underline underline-offset-1">Hatlabs</a>
         </Link>
       </h1>
     </>
