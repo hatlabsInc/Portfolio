12a8787a6f1bc4a5b9a51ddba09e506f122758ee
Palahana <ahana.pal@wowlabz.com>
2023-11-23 22:32:35 +0530

    Update(Work) : Added work and links

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -1,6 +1,7 @@
 import React from "react";
+import Button from "../Button";
 
-const WorkCard = ({ img, name, description, onClick }) => {
+const WorkCard = ({ img, name, description, site_url, github_url, onClick }) => {
   return (
     <div
       className="overflow-hidden rounded-lg p-2 laptop:p-4 first:ml-0 link"
@@ -10,18 +11,22 @@ const WorkCard = ({ img, name, description, onClick }) => {
         className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto"
         style={{ height: "300px" }}
       >
-        <img
+        <iframe
           alt={name}
           className="h-full w-full object-cover hover:scale-110 transition-all ease-out duration-300"
           src={img}
-        ></img>
+        ></iframe>
       </div>
       <h1 className="mt-5 text-3xl font-medium">
         {name ? name : "Project Name"}
       </h1>
+      <br/>
       <h2 className="text-xl opacity-50">
         {description ? description : "Description"}
       </h2>
+      <Button href={site_url}>Visit Site</Button>
+      <Button href={github_url}>Codebase</Button>
+
     </div>
   );
 };
