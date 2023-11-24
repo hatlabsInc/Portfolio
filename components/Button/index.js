161d8f97679fc25643ab3814a63546093b43ff28
DeagleOfficial <supratikofficial1@gmail.com>
2023-11-24 23:40:40 +0530

    Fixed button

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import data from "../../data/portfolio.json";
 import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
 
 const Button = ({ children, type, onClick, classes }) => {
 
   const {theme} = useTheme()
+
+  const [localTheme, setLocalTheme] = useState("dark")
+
+  useEffect(() => {
+    setLocalTheme(theme)
+  }, [theme])
 
   if (type === "primary") {
     return (
@@ -12,7 +19,7 @@ const Button = ({ children, type, onClick, classes }) => {
         onClick={onClick}
         type="button"
         className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg ${
-          theme === "dark" ? "bg-white text-black" : "bg-black text-white"
+          localTheme === "dark" ? "bg-white text-black" : "bg-black text-white"
         }  transition-all duration-300 ease-out first:ml-0 hover:scale-105 active:scale-100 link ${
           data.showCursor && "cursor-none"
         }  ${classes}`}
@@ -26,8 +33,8 @@ const Button = ({ children, type, onClick, classes }) => {
       onClick={onClick}
       type="button"
       className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg flex items-center transition-all ease-out duration-300  ${
-        theme === "dark"
-          ? "hover:bg-slate-600 text-white "
+        localTheme === "dark"
+          ? "hover:bg-slate-600 text-white"
           : "hover:bg-slate-100"
       } hover:scale-105 active:scale-100  tablet:first:ml-0  ${
         data.showCursor && "cursor-none"
