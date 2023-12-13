e4615c773ac9aaf3d03d9de1ccc6f6b6c60fe4bf
Hatlabs <151822455+hatlabsInc@users.noreply.github.com>
2023-12-13 11:36:39 +0530

    Adding google analytics to the website

--- pages/index.js
+++ pages/index.js
@@ -75,6 +75,15 @@ export default function Home() {
   }
 
   return (
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GWW24SZZ3T"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-GWW24SZZ3T');
+</script>
     <div
       className={`relative ${
         data.showCursor && "cursor-none"
